ef08d89b5e5b5f11b4f26e3c3b58e4caed6a10c5
Hardik Karena <hardikkarena77@gmail.com>
2020-09-03 04:20:35 -0700

    2nd

--- app/models.py
+++ app/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-from django.utils import timezone
 from django.conf import settings
-
+from django.utils import timezone
 # Create your models here.
-class post(models.Model):
+
+class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    title2 = models.CharField(max_length=200)
--- project/settings.py
+++ project/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_f%f^u_!p!b3f(=obf4fapz5)5(&#2fw^-9)1^=!z%jg@^bhb@'
+SECRET_KEY = 'q-5g$xzz$rdd6dps8(nj3=x5p75l3_&jdak+6)w3ip3%p3@8t8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'dgirls',
         'USER': 'root',
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Indian/Maldives'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
@@ -123,4 +123,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
